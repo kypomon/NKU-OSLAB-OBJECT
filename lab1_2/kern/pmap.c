107be88abe85e495730e82961f79b71c91c07b41
kypomon <48947751+kypomon@users.noreply.github.com>
2019-10-04 17:32:56 +0800

    lab2部分的代码上传
    对pamap.c代码进行了进一步完善了pgdir_walk()
    boot_map_region()
    page_lookup()
    page_remove()
    page_insert()
    mem_init()
    等函数的功能；
    此外还删除了之前的虚假文件。

--- lab1_2/kern/pmap.c
+++ lab1_2/kern/pmap.c
@@ -378,7 +378,32 @@ pte_t *
 pgdir_walk(pde_t *pgdir, const void *va, int create)
 {
 	// Fill this function in
-	return NULL;
+
+    pde_t *pde = NULL;
+    pte_t *pgtable = NULL;
+
+    struct PageInfo *pp = NULL;
+
+    pde = &pgdir[PDX(va)];
+
+    if(*pde & PTE_P)
+    {
+        pgtable = (KADDR(PTE_ADDR(*pde)));
+    }
+    else
+    {
+        if(!create ||
+            !(pp = page_alloc(ALLOC_ZERO)) ||
+            !(pgtable = (pte_t *)page2kva(pp)))
+        {
+            return NULL;
+        }
+
+        pp->pp_ref++;
+        *pde = PADDR(pgtable) | PTE_P | PTE_W | PTE_U;
+    }
+
+	return &pgtable[PTX(va)];
 }
 
 //
@@ -396,6 +421,29 @@ static void
 boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm)
 {
 	// Fill this function in
+    uintptr_t  va_next = va;
+    physaddr_t pa_next = pa;
+    pte_t       *pte   = NULL;
+
+    ROUNDUP(size, PGSIZE);
+
+    assert(size % PGSIZE == 0 || cprintf("size : %x \n", size));
+
+    int temp = 0;
+
+    for(temp = 0; temp < size/PGSIZE; temp++)
+    {
+        pte = pgdir_walk(pgdir, (void*)va_next, 1);
+
+        if(!pte)
+        {
+            return ;
+        }
+
+        *pte = PTE_ADDR(pa_next) | perm | PTE_P;
+        pa_next += PGSIZE;
+        va_next += PGSIZE;
+    }
 }
 
 //
@@ -427,6 +475,35 @@ int
 page_insert(pde_t *pgdir, struct PageInfo *pp, void *va, int perm)
 {
 	// Fill this function in
+    pte_t *pte = pgdir_walk(pgdir, va, 0);
+    physaddr_t ppa = page2pa(pp);
+
+    if(pte)
+    {
+        if(*pte & PTE_P)
+        {
+            page_remove(pgdir, va);
+        }
+
+        if(page_free_list == pp)
+        {
+            page_free_list = page_free_list->pp_link;
+        }
+    }
+    else
+    {
+        pte = pgdir_walk(pgdir, va, 1);
+        if(!pte)
+        {
+            return -E_NO_MEM;
+        }
+
+    }
+
+    *pte = page2pa(pp) | PTE_P | perm;
+
+    pp->pp_ref++;
+    tlb_invalidate(pgdir, va);
 	return 0;
 }
 
@@ -445,7 +522,16 @@ struct PageInfo *
 page_lookup(pde_t *pgdir, void *va, pte_t **pte_store)
 {
 	// Fill this function in
-	return NULL;
+    pte_t * pte = pgdir_walk(pgdir, va, 0);
+
+    if(!pte)
+    {
+        return NULL;
+    }
+
+    *pte_store = pte;
+
+	return pa2page(PTE_ADDR(*pte));
 }
 
 //
@@ -467,6 +553,19 @@ void
 page_remove(pde_t *pgdir, void *va)
 {
 	// Fill this function in
+    pte_t *pte = pgdir_walk(pgdir, va, 0);
+    pte_t ** pte_store = &pte;
+
+    struct PageInfo *pp = page_lookup(pgdir, va, pte_store);
+
+    if(!pp)
+    {
+        return ;
+    }
+
+    page_decref(pp);
+    **pte_store = 0;
+    tlb_invalidate(pgdir, va);
 }
 
 //
